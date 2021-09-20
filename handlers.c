9411dd130e111ff4ec08f8163c1d3b60b7c53c23
Herbrant <cdavide98carnemolla@gmail.com>
2021-09-21 01:45:46 +0200

    refactor: handlers

--- handlers.c
+++ handlers.c
@@ -1,65 +1,69 @@
 #include "handlers.h"
 
-void callback_coffee_get_by_date(json_t* json_request, json_t* json_response, void* db){
-    json_t* json_date = json_object_get(json_request, "date");
-    const char* date = json_string_value(json_date);
-
-    if(!date){
-        json_object_set_new(json_response, "status", json_string("error"));
-        return;
-    }
-    
+void get_coffee_counter(const char* date, json_t* json_response, void* db){
     int counter = getCoffeeCounterByDate(db, date);
     json_object_set_new(json_response, "status", json_string("success"));
     json_object_set_new(json_response, "counter", json_integer(counter));
 }
 
-void callback_add_coffee(json_t* json_request, json_t* json_response, void* db){
-    json_t* json_date = json_object_get(json_request, "date");
-    const char* date = json_string_value(json_date);
+void callback_get_coffee_counter(char today, json_t* json_request, json_t* json_response, void* db){
+    if(today){
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        char date[11];
 
-    if(!date)
-        json_object_set_new(json_response, "status", json_string("error"));
-    else if(addCoffee(db, date) != 0)
-        json_object_set_new(json_response, "status", json_string("error"));
-    else
-        json_object_set_new(json_response, "status", json_string("success"));
+        if(snprintf(date, 11, "%d-%02d-%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday) <= 0){
+            json_object_set_new(json_response, "status", json_string("error"));
+            return;
+        }
+
+        get_coffee_counter(date, json_response, db);
+    }
+    else{
+        json_t* json_date = json_object_get(json_request, "date");
+        const char* date = json_string_value(json_date);
+
+        if(!date){
+            json_object_set_new(json_response, "status", json_string("error"));
+            return;
+        }
+
+        get_coffee_counter(date, json_response, db);
+    }
 }
 
-void callback_add_today_coffee(json_t* json_request, json_t* json_response, void* db){
-    time_t t = time(NULL);
-  	struct tm tm = *localtime(&t);
-    char date[11];
-
-    if(snprintf(date, 11, "%d-%02d-%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday) <= 0){
-        json_object_set_new(json_response, "status", json_string("error"));
-        return;
-    }
-
-    printf("%s\n", date);
-    
-    initCoffeeDay(db);
-
+void add_coffee(const char* date, json_t* json_response, void* db){
     if(addCoffee(db, date) != 0)
         json_object_set_new(json_response, "status", json_string("error"));
     else
         json_object_set_new(json_response, "status", json_string("success"));
 }
 
-void callback_get_today_coffee(json_t* json_request, json_t* json_response, void* db){
-    time_t t = time(NULL);
-  	struct tm tm = *localtime(&t);
-    char date[11];
+void callback_add_coffee(char today, json_t* json_request, json_t* json_response, void* db){
+    if(today){
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        char date[11];
 
-    if(snprintf(date, 11, "%d-%02d-%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday) <= 0){
-        json_object_set_new(json_response, "status", json_string("error"));
-        return;
+        if(snprintf(date, 11, "%d-%02d-%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday) <= 0){
+            json_object_set_new(json_response, "status", json_string("error"));
+            return;
+        }
+
+        initCoffeeDay(db);
+        add_coffee(date, json_response, db);
     }
-    
-    int counter = getCoffeeCounterByDate(db, date);
-    json_object_set_new(json_response, "status", json_string("success"));
-    json_object_set_new(json_response, "counter", json_integer(counter));
+    else{
+        json_t* json_date = json_object_get(json_request, "date");
+        const char* date = json_string_value(json_date);
+
+        if(!date)
+            json_object_set_new(json_response, "status", json_string("error"));
+
+        add_coffee(date, json_response, db);
+    }
 }
+
 
 int callback_coffee(const struct _u_request* request, struct _u_response* response, void* db){
     json_t* json_request;
@@ -83,19 +87,19 @@ int callback_coffee(const struct _u_request* request, struct _u_response* respon
     const char* operation = json_string_value(json_operation);
 
     if(!strcmp(operation, "get_by_date")){
-        callback_coffee_get_by_date(json_request, json_response, db);
+        callback_get_coffee_counter(0, json_request, json_response, db);
         ulfius_set_json_body_response(response, 200, json_response);
     }
-    else if(!strcmp(operation, "add")){
-        callback_add_coffee(json_request, json_response, db);
+    else if(!strcmp(operation, "add_coffee")){
+        callback_add_coffee(0, json_request, json_response, db);
         ulfius_set_json_body_response(response, 200, json_response);
     }
     else if(!strcmp(operation, "add_today_coffee")){
-        callback_add_today_coffee(json_request, json_response, db);
+        callback_add_coffee(1, json_request, json_response, db);
         ulfius_set_json_body_response(response, 200, json_response);
     }
     else if(!strcmp(operation, "get_today_coffee")){
-        callback_get_today_coffee(json_request, json_response, db);
+        callback_get_coffee_counter(1, json_request, json_response, db);
         ulfius_set_json_body_response(response, 200, json_response);
     }
     else{
--- handlers.h
+++ handlers.h
@@ -6,10 +6,10 @@
 #include "database.h"
 
 int callback_coffee(const struct _u_request* request, struct _u_response* response, void* db);
-void callback_coffee_get_by_date(json_t* json_request, json_t* json_response, void* db);
-void callback_add_coffee(json_t* json_request, json_t* json_response, void* db);
-void callback_add_today_coffee(json_t* json_request, json_t* json_response, void* db);
-void callback_get_today_coffee(json_t* json_request, json_t* json_response, void* db);
+void get_coffee_counter(const char* date, json_t* json_response, void* db);
+void callback_get_coffee_counter(char today, json_t* json_request, json_t* json_response, void* db);
+void add_coffee(const char* date, json_t* json_response, void* db);
+void callback_add_coffee(char today, json_t* json_request, json_t* json_response, void* db);
 
 
 #endif
